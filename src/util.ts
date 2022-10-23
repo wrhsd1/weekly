5611bcc499838627a77017fbc2766d9fc22b6962
wrhsd1 <wangrenhuan@gmail.com>
2022-10-23 10:09:41 +0800

    Update util.ts

--- src/util.ts
+++ src/util.ts
@@ -1,3 +1,14 @@
+// 转化成标题
+export const parseTitle = (currentPage: string) => {
+    const oldTitle = decodeURIComponent(currentPage.split('/posts/')[1]);
+    let title = [1];
+    if (title.slice(-1) == '/') {
+        title = title.substring(0, title.length - 1);
+    }
+    return title
+}
+
+
 //排序所有的文章
 export const sortPosts = (allPosts: any) => {
     return allPosts.sort((a, b) => {
