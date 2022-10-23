33fc8f64ee95e593370a507cc5d27a9181c4ccdb
wrhsd1 <wangrenhuan@gmail.com>
2022-10-23 13:38:10 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -6,7 +6,7 @@ export const SITE = {
   icon: 'https://gw.alipayobjects.com/zos/k/qv/coffee-2-icon.png',
   pic: 'https://cdn.fliggy.com/upic/2XFNGI.png',
   homePage: 'https://weekly.tw93.fun',
-  blogPage: 'https://tw93.fun/',
+  blogPage: 'https://wrhcn.eu.org/',
   twitterId: 'HiTw93',
   githubId: 'tw93',
   repo: 'tw93/weekly'
