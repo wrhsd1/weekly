a00e408529d69f452cb3b59543c194645a655a52
wrhsd1 <wangrenhuan@gmail.com>
2022-10-23 10:06:11 +0800

    Update util.ts

--- src/util.ts
+++ src/util.ts
@@ -1,13 +1,3 @@
-// 转化成标题
-export const parseTitle = (currentPage: string) => {
-    const oldTitle = decodeURIComponent(currentPage.split('/posts/')[1]);
-    let title = '第' + oldTitle.split('-')[0] + '期 - ' + oldTitle.split('-')[1];
-    if (title.slice(-1) == '/') {
-        title = title.substring(0, title.length - 1);
-    }
-    return title
-}
-
 //排序所有的文章
 export const sortPosts = (allPosts: any) => {
     return allPosts.sort((a, b) => {
