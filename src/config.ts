d5fe65fc940965f116a94e53dcd901107888ae74
wrhsd1 <wangrenhuan@gmail.com>
2022-10-24 08:09:51 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -4,7 +4,7 @@ export const SITE = {
   description: '随手搬运西方主流媒体的所谓的民主自由的报道，让帝国主义的丑恶嘴脸无处遁形~~~',
   keywords: '西方,帝国主义,丑恶嘴脸',
   icon: 'https://wangpan1-1252857045.file.myqcloud.com/Fake-News.png',
-  pic: 'https://wangpan1-1252857045.file.myqcloud.com/1111.png',
+  pic: 'https://wangpan1-1252857045.file.myqcloud.com/2222.png',
   homePage: 'https://weekly.tw93.fun',
   blogPage: 'https://wrhcn.eu.org/',
   twitterId: 'HiTw93',
